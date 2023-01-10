2961e8466d5b8f8a3fc92dff30e81ca5009b4f3a
kaustubhgit07 <122269605+kaustubhgit07@users.noreply.github.com>
2023-01-10 09:44:48 +0530

    Add files via upload

---
+++ r lang/practisequestionsR.R
@@ -0,0 +1,24 @@
+ 
+#R Programming: Basic Exercise
+#Write a program to get the details of the objects in memory
+name="R"
+n1=10;
+n2=0.5
+nums=c(10,20,30,40,50)
+print(ls())
+print("details of the objects in memory:")
+print(ls.str())
+?ls
+
+sample(c("H", "T"), 20, replace=T)
+resample(c("H", "T"), 20, replace=T)
+?sample
+sink.number(type = "blah blah")
+?sink
+v <- 2*x + y + 4
+v
+v <- 2*x + y + 1
+v <- 2*x + y + 1
+v
+v <- 2*x + y + 1
+v
